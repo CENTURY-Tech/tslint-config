8e62078cbc203e3fb063d1e0193f631828016419
Iain Reid <iainreid382@gmail.com>
2019-07-25 15:07:34 +0100

    Update config

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  extends: "tslint:latest",
+  rules: {
+    "no-namespace": [
+      false
+    ],
+    "member-access": [
+      true
+    ],
+    "object-literal-shorthand": false,
+    "trailing-comma": [
+      false
+    ],
+    "no-angle-bracket-type-assertion": false,
+    "object-literal-sort-keys": false,
+    "whitespace": [
+      false
+    ],
+    "no-consecutive-blank-lines": [
+      true
+    ],
+    "no-console": [
+      false
+    ],
+    "no-string-literal": false,
+    "ban-types": false,
+    "only-arrow-functions": [
+      false
+    ],
+    "interface-name": [
+      true,
+      "never-prefix"
+    ],
+    "no-reference": false,
+    "prefer-conditional-expression": false
+  }
+};
